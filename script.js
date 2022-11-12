ab08b32cc289049a2a4fb2d3361a5f5d2d66e845
ADRIAN <38044318+KOLANOADRIAN@users.noreply.github.com>
2022-11-12 22:19:54 +0100

    added cookies alert to web

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 window.addEventListener("load", function () {
     console.log("HelloWorld!... The page has loaded")
 });
@@ -15,3 +16,32 @@ const start = () => {
 
 const container_startButton = document.querySelector(".container_startButton");
 container_startButton.addEventListener("click", start);
+
+
+// ...........cookies_alert
+const alert = () => {
+    const cookies_alert = document.querySelector(".cookies_alert");
+    cookies_alert.remove();
+    const read = document.querySelector(".read");
+    read.remove();
+};
+
+const button_akcept = document.querySelector(".button_akcept");
+button_akcept.addEventListener("click", alert)
+
+const info = () => {
+    const read = document.querySelector(".read");
+    read.classList.toggle("read_open");
+};
+const button_info = document.querySelector(".button_info");
+button_info.addEventListener("click", info);
+
+
+// 
+const body = document.querySelector(".body");
+const button_mode = document.querySelector(".button_mode");
+const themeName = document.querySelector(".themeName")
+button_mode.addEventListener("click", () => {
+    body.classList.toggle("body_dark");
+    themeName.innerText = body.classList.contains("body_dark") ? "jasny" : "ciemny"
+});
